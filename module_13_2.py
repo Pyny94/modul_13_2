6b999e717fec490b8dd3664d29e36ac106b30be1
Vladislav <pyny94@mail.ru>
2024-09-17 14:11:23 +0300

    Add files via upload

--- module_13_2.py
+++ module_13_2.py
@@ -6,7 +6,7 @@ from aiogram.filters.command import Command
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="72342421")
+bot = Bot(token="736nbESY8Kmuw_Wl4")
 # Диспетчер
 dp = Dispatcher()
 
@@ -14,15 +14,20 @@ dp = Dispatcher()
 # Гендлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    print('Привет! Я бот помогающий твоему здоровью.')
 
 
 
 @dp.message()
 async def all_message(message: types.Message):
-    await message.reply('Введите команду /start, чтобы начать общение.')
+    print('Введите команду /start, чтобы начать общение.')
 
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
